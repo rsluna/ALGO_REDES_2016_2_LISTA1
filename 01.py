215686861e4af80d66d7cafe11ade92197780c8a
Rodrigo Sérgio de Luna <rodrigosluna@gmail.com>
2016-08-18 13:23:52 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,37 @@
+#questão 01
+print('Alô Mundo estou programando em Python')
+print(''
+      '')
+
+#Questão 02
+frase=('Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.( Martin Luther King Junior)')
+print(frase.upper())
+print(frase.lower())
+print(frase.title())
+print(''
+      '')
+
+#Questão03
+#Soma
+soma=int(200+250)
+print('O valor da soma é: ',soma)
+print(''
+      '')
+#Divisão
+divisao=int(200/8)
+print('O valor da divisão é: ',divisao)
+print(''
+      '')
+#Multiplicação
+multiplica=int(200*7)
+print('O valor da multiplicação é: ',multiplica)
+print(''
+      '')
+#Subtração
+subtrai=int(800-10)
+print('O Valor da Subtração é: ',subtrai)
+print(''
+      '')
+#Potencia
+potença=int(2**10)
+print('O valor da potência é: ',potença)
